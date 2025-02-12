2f79862b7d027c6b1d6fc13167d834021c0a2ffd
muritador5050 <muritador5050@gmail.com>
2025-02-13 00:01:04 +0100

    set media query

--- src/App.js
+++ src/App.js
@@ -1,24 +1,150 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+export function App() {
+  const [weather, setWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [region, setRegion] = useState('Mecca');
+  const [changeLocation, setChangeLocation] = useState(false);
+
+  //Local Time
+  function getDayName(date = new Date(), locale = 'en-US') {
+    return date.toLocaleDateString(locale, { weekday: 'long' });
+  }
+
+  async function handleGetWeather() {
+    setIsLoading(true);
+    const options = {
+      method: 'GET',
+      url: `http://api.weatherapi.com/v1/current.json?q=${region}&aqi=no`,
+      headers: {
+        key: 'b4d344549f2140059e4125539230405',
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+      const result = response.data;
+      setWeather(result);
+    } catch (error) {
+      console.error(error);
+    }
+    setIsLoading(false);
+  }
+
+  function setCountry() {
+    const options = {
+      method: 'POST',
+      url: `http://api.weatherapi.com/v1/current.json?q=${region}&aqi=no`,
+      headers: {
+        key: 'b4d344549f2140059e4125539230405',
+        'Access-Control-Allow-Origin': '*',
+      },
+    };
+    const response = axios
+      .request(options)
+      .then((response) => setWeather(response.data));
+    console.log(response.data);
+  }
+
+  const handleChangeLocation = () => {
+    setCountry();
+    setChangeLocation(false);
+  };
+  useEffect(() => {
+    handleGetWeather();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && <h3 style={{ textAlign: 'center' }}>Loading...</h3>}
+      <div className='container'>
+        {/* <h2 className='weather-template'>Mini Weather App</h2> */}
+        {weather && (
+          <>
+            <div className='weather-blue-template'>
+              <div className='location'>
+                {' '}
+                <ul>
+                  <li>
+                    <h3>{getDayName()}</h3>
+                  </li>
+                  <li>{weather.location.localtime}</li>
+
+                  <li>
+                    {weather.location.name}/{weather.location.country}
+                  </li>
+                </ul>
+              </div>
+              <div className='condition'>
+                <ul>
+                  <li>
+                    <img
+                      className='icon'
+                      src={weather.current.condition.icon}
+                      alt='icon'
+                    />
+                  </li>
+                  <li>
+                    {' '}
+                    <h1>
+                      {weather.current.temp_c} <sup>0</sup>C{' '}
+                    </h1>
+                  </li>
+                  <li style={{ fontWeight: 'bolder' }}>
+                    {' '}
+                    {weather.current.condition.text}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className='weather-black-template'>
+              <ul>
+                <li className='list-condition'>
+                  <strong>HUMIDITY</strong> {weather.current.humidity}
+                </li>
+                <li className='list-condition'>
+                  <strong>WIND_DIR</strong>
+                  {weather.current.wind_dir}
+                </li>
+                <li className='list-condition'>
+                  <strong>WIND</strong>
+                  {weather.current.wind_kph}km/h
+                </li>
+                <li className='list-condition'>
+                  <strong>CLOUD</strong>
+                  {weather.current.cloud}
+                </li>
+              </ul>
+              {changeLocation ? (
+                <div className='form-div'>
+                  <input
+                    className='text-input'
+                    type='text'
+                    value={region}
+                    onChange={(e) => setRegion(e.target.value)}
+                  />
+                  <button className='go-button' onClick={handleChangeLocation}>
+                    go
+                  </button>
+                </div>
+              ) : (
+                ''
+              )}
+              <button
+                className='change-location-btn'
+                onClick={() => setChangeLocation(true)}
+              >
+                Change Location
+              </button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
